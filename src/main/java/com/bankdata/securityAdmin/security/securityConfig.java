0e61cdf40798f10d86350cb9ff2e46af1bf61ca9
Edwin-Antonio <edanarellanoma@ittepic.edu.mx>
2024-01-31 06:57:28 -0600

    Denied page access based in roles

--- src/main/java/com/bankdata/securityAdmin/security/securityConfig.java
+++ src/main/java/com/bankdata/securityAdmin/security/securityConfig.java
@@ -14,8 +14,8 @@ public class securityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails miss1 = User.builder().username("jhon").password("{noop}123").roles("EMPLOYEE").build();
-        UserDetails miss2 = User.builder().username("maria").password("{noop}123").roles("EMPLOYEE, ADMIN").build();
-        UserDetails miss3 = User.builder().username("pablo").password("{noop}123").roles("EMPLOYEE, MANAGER").build();
+        UserDetails miss2 = User.builder().username("maria").password("{noop}123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+        UserDetails miss3 = User.builder().username("pablo").password("{noop}123").roles("EMPLOYEE", "MANAGER").build();
         return new InMemoryUserDetailsManager(miss1,miss2,miss3);
     }
 
@@ -23,13 +23,19 @@ public class securityConfig {
     @Bean
     public SecurityFilterChain filterChainLogin(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configurer -> configurer
+                //Asignando roles de usuarios Empleado, Manager o Administrador
+                .requestMatchers("/").hasRole("EMPLOYEE")
+                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                .requestMatchers("/system/**").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated())
                 .formLogin(form -> form
                         .loginPage("/personalLoginPage")
+                        
                         //Proceso para autentificar un usuario
                         .loginProcessingUrl("/authenticateTheUser")
                         .permitAll())
+                        
                         //Permiso para generar un logout
                         .logout(logout -> logout
                         .permitAll());
