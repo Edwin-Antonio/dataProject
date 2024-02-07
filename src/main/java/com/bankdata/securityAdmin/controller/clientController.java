459b28d7fa4ad9489cb038e8bde7fbd3d5ffb872
Edwin-Antonio <edanarellanoma@ittepic.edu.mx>
2024-02-06 23:27:23 -0600

    Added a new Update method/button for Clients

--- src/main/java/com/bankdata/securityAdmin/controller/clientController.java
+++ src/main/java/com/bankdata/securityAdmin/controller/clientController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bankdata.securityAdmin.entity.Client;
 import com.bankdata.securityAdmin.service.clientService;
@@ -41,10 +42,22 @@ public class clientController {
         return "addClientForm";
     }
 
+    @GetMapping("/formUpdateClient")
+    public String updateClient(@RequestParam("clientId") int theId, Model model){
+        //Obtenemos el servicio del cliente
+        Client theClient = clientService.findById(theId);
+        // insertamos al cliente en el modelo **NOTA** Si se trata de actualizar forzosamente en nuestro formulario debemos poner un campo
+        // con el ID de nuestro objeto de forma escondida
+        model.addAttribute("addClient",theClient); // El atributo que colocamos aquí es el que usamos para nuestro metodo guardar
+        // mandamos la informacion a nuestro formulario
+        return "/addClientForm";
+    }
+
     @PostMapping("/save")
     public String saveClient(@ModelAttribute("addClient") Client theClient){
         clientService.save(theClient);
         return "redirect:/list";
     }
+
 
 }
